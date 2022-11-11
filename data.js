55c2001217aeefb338756d46ad2369808ec7fe34
Michael Cronin <mcronin02@gmail.com>
2022-11-11 17:12:57 -0500

    Update data.js

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ var data = [
         "approximate": "31 Oct morning",
         "date": "31 Oct 5a",
         "level": null,
+        "radiance": 200,
         "low": 4,
         "low_int": 5,
         "high_int": 7
@@ -11,6 +12,7 @@ var data = [
         "approximate": "31 Oct afternoon",
         "date": "Oct 31 3p",
         "level": 16,
+        "radiance": null,
         "low": 5,
         "low_int":6,
         "high_int":8
@@ -19,6 +21,7 @@ var data = [
         "approximate": "31 Oct evening",
         "date": "Oct 31 10p",
         "level": 14.7,
+        "radiance": null,
         "low": null,
         "low_int":null,
         "high_int":null
@@ -27,6 +30,7 @@ var data = [
         "approximate": "1 Nov morning",
         "date": "1 Nov 7a",
         "level": 9.2,
+        "radiance": null,
         "low": 7,
         "low_int": 9,
         "high_int": 11
@@ -35,6 +39,7 @@ var data = [
         "approximate": "1 Nov afternoon",
         "date": "1 Nov 3p",
         "level": 8.8,
+        "radiance": null,
         "low": 9,
         "low_int": 11,
         "high_int": 13
@@ -42,6 +47,7 @@ var data = [
     {
         "approximate": "1 Nov evening",
         "level": null,
+        "radiance": null,
         "low": 9.5,
         "low_int": 13,
         "high_int": 15
@@ -50,6 +56,7 @@ var data = [
         "approximate": "2 Nov morning",
         "date": "2 Nov 5a",
         "level": 12,
+        "radiance": null,
         "low": null,
         "low_int": null,
         "high_int": null
@@ -58,6 +65,7 @@ var data = [
         "approximate": "2 Nov afternoon",
         "date": "2 Nov 1p",
         "level": 13.7,
+        "radiance": 120,
         "low": 11.1,
         "low_int": 13.3,
         "high_int": 16
@@ -66,6 +74,7 @@ var data = [
         "approximate": "2 Nov evening",
         "date": "2 Nov 7p",
         "level": 13.3,
+        "radiance": null,
         "low": null,
         "low_int": null,
         "high_int": null
@@ -74,6 +83,7 @@ var data = [
         "approximate": "3 Nov morning",
         "date": "3 Nov 5a",
         "level": 13.3,
+        "radiance": null,
         "low": 11.8,
         "low_int": 14.8,
         "high_int": 16.8
@@ -82,6 +92,7 @@ var data = [
         "approximate": "3 Nov afternoon",
         "date": "3 Nov 12p",
         "level": 13,
+        "radiance": 90,
         "low": 12.4,
         "low_int": 15.1,
         "high_int": 17.3
@@ -90,6 +101,7 @@ var data = [
         "approximate": "3 Nov evening",
         "date": "3 Nov 6p",
         "level": 12.4,
+        "radiance": 80,
         "low": null,
         "low_int": null,
         "high_int": null
@@ -98,6 +110,7 @@ var data = [
         "approximate": "4 Nov morning",
         "date": "4 Nov 6a",
         "level": 11.3,
+        "radiance": 0,
         "low": 12.8,
         "low_int": 15.5,
         "high_int": 17.5
@@ -105,6 +118,7 @@ var data = [
     {
         "approximate": "4 Nov afternoon",
         "level": null,
+        "radiance": null,
         "low": 13,
         "low_int": 16,
         "high_int": 17.8
@@ -113,6 +127,7 @@ var data = [
         "approximate": "4 Nov evening",
         "date": "4 Nov 6p",
         "level": 13.9,
+        "radiance": 0,
         "low": null,
         "low_int": null,
         "high_int": null
@@ -121,6 +136,7 @@ var data = [
         "approximate": "5 Nov morning",
         "date": "5 Nov 6a",
         "level": 15.5,
+        "radiance": 120,
         "low": 13,
         "low_int": 15.5,
         "high_int": 17.5
@@ -129,6 +145,7 @@ var data = [
         "approximate": "5 Nov afternoon",
         "date": "5 Nov 12:40p",
         "level": 16.6,
+        "radiance": null,
         "low": 13,
         "low_int": 15.2,
         "high_int": 17.3
@@ -136,71 +153,151 @@ var data = [
     {
         "approximate": "5 Nov evening",
         "date": "5 Nov 11: 30p",
-        "level": null
+        "level": null,
+        "radiance": 90
     },
     {
         "approximate": "6 Nov morning",
         "date": "6 Nov 5a",
-        "level": null
+        "level": null,
+        "radiance": null
     },
     {
         "approximate": "6 Nov afternoon",
-        "level": null
+        "level": null,
+        "radiance": null
     },
     {
         "approximate": "6 Nov evening",
         "date": "6 Nov 5p",
-        "level": null
+        "level": null,
+        "radiance": 0
     },
     {
         "approximate": "7 Nov morning",
         "date": "7 Nov 5a",
-        "level": 10.9
+        "level": 10.9,
+        "radiance": null
     },
     {
         "approximate": "7 Nov afternoon",
-        "level": null
+        "level": null,
+        "radiance": null
     },
     {
         "approximate": "7 Nov evening",
         "date": "7 Nov 5p",
-        "level": 12
+        "level": 12,
+        "radiance": null
     },
     {
         "approximate": "8 Nov morning",
         "date": "8 Nov 5a",
-        "level": 14.4
+        "level": 14.4,
+        "radiance": 60
     },
     {
         "approximate": "8 Nov afternoon",
-        "level": null
+        "level": null,
+        "radiance": null
     },
     {
         "approximate": "8 Nov evening",
         "date": "8 Nov 5p",
-        "level": null
+        "level": null,
+        "radiance": null
     },
     {
         "approximate": "9 Nov morning",
         "date": "9 Nov 5a",
-        "level": 16.3
+        "level": 16.3,
+        "radiance": null
     },
     {
         "approximate": "9 Nov afternoon",
-        "level": null
+        "level": null,
+        "radiance": null
     },
     {
         "approximate": "9 Nov evening",
-        "level": null
+        "level": null,
+        "radiance": null
     },
     {
         "approximate": "10 Nov morning",
         "date": "10 Nov 5a",
-        "level": 16.7
+        "level": 16.7,
+        "radiance": null
     },
     {
         "approximate": "10 Nov afternoon",
         "actual": "10 Nov 3p",
-        "level": 17.3
+        "level": 17.3,
+        "radiance": null
+    },
+    {
+        "approximate": "10 Nov evening",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "11 Nov morning",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "11 Nov afternoon",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "11 Nov evening",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "12 Nov morning",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "12 Nov afternoon",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "12 Nov evening",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "13 Nov morning",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "13 Nov afternoon",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "13 Nov evening",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "14 Nov morning",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "14 Nov afternoon",
+        "level": null,
+        "radiance": null
+    },
+    {
+        "approximate": "14 Nov evening",
+        "level": null,
+        "radiance": null
     }
 ]
