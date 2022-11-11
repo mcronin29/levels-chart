a76b0c1abd1d841595265437dc34d8b8894ced3e
Michael Cronin <mcronin02@gmail.com>
2022-11-11 14:44:54 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,206 @@
+var data = [
+    {
+        "approximate": "31 Oct morning",
+        "date": "31 Oct 5a",
+        "level": null,
+        "low": 4,
+        "low_int": 5,
+        "high_int": 7
+    },
+    {
+        "approximate": "31 Oct afternoon",
+        "date": "Oct 31 3p",
+        "level": 16,
+        "low": 5,
+        "low_int":6,
+        "high_int":8
+    },
+    {
+        "approximate": "31 Oct evening",
+        "date": "Oct 31 10p",
+        "level": 14.7,
+        "low": null,
+        "low_int":null,
+        "high_int":null
+    },
+    {
+        "approximate": "1 Nov morning",
+        "date": "1 Nov 7a",
+        "level": 9.2,
+        "low": 7,
+        "low_int": 9,
+        "high_int": 11
+    },
+    {
+        "approximate": "1 Nov afternoon",
+        "date": "1 Nov 3p",
+        "level": 8.8,
+        "low": 9,
+        "low_int": 11,
+        "high_int": 13
+    },
+    {
+        "approximate": "1 Nov evening",
+        "level": null,
+        "low": 9.5,
+        "low_int": 13,
+        "high_int": 15
+    },
+    {
+        "approximate": "2 Nov morning",
+        "date": "2 Nov 5a",
+        "level": 12,
+        "low": null,
+        "low_int": null,
+        "high_int": null
+    },
+    {
+        "approximate": "2 Nov afternoon",
+        "date": "2 Nov 1p",
+        "level": 13.7,
+        "low": 11.1,
+        "low_int": 13.3,
+        "high_int": 16
+    },
+    {
+        "approximate": "2 Nov evening",
+        "date": "2 Nov 7p",
+        "level": 13.3,
+        "low": null,
+        "low_int": null,
+        "high_int": null
+    },
+    {
+        "approximate": "3 Nov morning",
+        "date": "3 Nov 5a",
+        "level": 13.3,
+        "low": 11.8,
+        "low_int": 14.8,
+        "high_int": 16.8
+    },
+    {
+        "approximate": "3 Nov afternoon",
+        "date": "3 Nov 12p",
+        "level": 13,
+        "low": 12.4,
+        "low_int": 15.1,
+        "high_int": 17.3
+    },
+    {
+        "approximate": "3 Nov evening",
+        "date": "3 Nov 6p",
+        "level": 12.4,
+        "low": null,
+        "low_int": null,
+        "high_int": null
+    },
+    {
+        "approximate": "4 Nov morning",
+        "date": "4 Nov 6a",
+        "level": 11.3,
+        "low": 12.8,
+        "low_int": 15.5,
+        "high_int": 17.5
+    },
+    {
+        "approximate": "4 Nov afternoon",
+        "level": null,
+        "low": 13,
+        "low_int": 16,
+        "high_int": 17.8
+    },
+    {
+        "approximate": "4 Nov evening",
+        "date": "4 Nov 6p",
+        "level": 13.9,
+        "low": null,
+        "low_int": null,
+        "high_int": null
+    },
+    {
+        "approximate": "5 Nov morning",
+        "date": "5 Nov 6a",
+        "level": 15.5,
+        "low": 13,
+        "low_int": 15.5,
+        "high_int": 17.5
+    },
+    {
+        "approximate": "5 Nov afternoon",
+        "date": "5 Nov 12:40p",
+        "level": 16.6,
+        "low": 13,
+        "low_int": 15.2,
+        "high_int": 17.3
+    },
+    {
+        "approximate": "5 Nov evening",
+        "date": "5 Nov 11: 30p",
+        "level": null
+    },
+    {
+        "approximate": "6 Nov morning",
+        "date": "6 Nov 5a",
+        "level": null
+    },
+    {
+        "approximate": "6 Nov afternoon",
+        "level": null
+    },
+    {
+        "approximate": "6 Nov evening",
+        "date": "6 Nov 5p",
+        "level": null
+    },
+    {
+        "approximate": "7 Nov morning",
+        "date": "7 Nov 5a",
+        "level": 10.9
+    },
+    {
+        "approximate": "7 Nov afternoon",
+        "level": null
+    },
+    {
+        "approximate": "7 Nov evening",
+        "date": "7 Nov 5p",
+        "level": 12
+    },
+    {
+        "approximate": "8 Nov morning",
+        "date": "8 Nov 5a",
+        "level": 14.4
+    },
+    {
+        "approximate": "8 Nov afternoon",
+        "level": null
+    },
+    {
+        "approximate": "8 Nov evening",
+        "date": "8 Nov 5p",
+        "level": null
+    },
+    {
+        "approximate": "9 Nov morning",
+        "date": "9 Nov 5a",
+        "level": 16.3
+    },
+    {
+        "approximate": "9 Nov afternoon",
+        "level": null
+    },
+    {
+        "approximate": "9 Nov evening",
+        "level": null
+    },
+    {
+        "approximate": "10 Nov morning",
+        "date": "10 Nov 5a",
+        "level": 16.7
+    },
+    {
+        "approximate": "10 Nov afternoon",
+        "actual": "10 Nov 3p",
+        "level": 17.3
+    }
+]
